471f74f4b44cf657e84b14480f3ed2763161549d
thanhtt-3322 <SUN-ASTERISK\tran.trung.thanh@b123322-pc.sun-asterisk.com>
2023-08-30 13:52:48 +0700

    search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items, dependent: :destroy
+  has_one :action_text_rich_text, class_name: "ActionText::RichText", as: :record
 
   validates :name, presence: true, length: { maximum: Settings.product.validates.name.max }
   validates :price, presence: true, numericality: { greater_than: Settings.product.validates.price.min,
@@ -12,6 +13,11 @@ class Product < ApplicationRecord
   scope :recommended, -> { order("RAND()").limit(Settings.display.slides.product_items) }
   scope :best_sellers, -> { order(sold: :desc).limit(Settings.display.slides.product_items) }
   scope :new_arrival, -> { order(created_at: :desc).limit(Settings.display.slides.product_items) }
+  scope :search, -> (search_term) {
+    joins(:action_text_rich_text, :category)
+      .where("products.name LIKE :search OR action_text_rich_texts.body LIKE :search OR categories.name LIKE :search",
+      search: "%#{search_term}%")
+  }
 
   has_one_attached :image, dependent: :destroy
   has_rich_text :body
